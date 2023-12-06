2dbed5fc92755147baeb49b94689cd902f59049d
Shavindu Thushara Sampath <83924562+ShavinduDeveloper@users.noreply.github.com>
2023-12-06 20:48:52 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta name="description" content="Shavindu Thushara Sampath">
+        <meta name="keywords" content="Font, font, Font Download, fontdownload, FONT, FontDownload, Best Fonts, Fonts, fonts,">
+        <title>Font Store | Free to Download</title>
+
+        <!--Title bar icon-->
+        <link rel ="icon" href ="Icon.png" type ="image/x-icon">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+
+        <link href="Style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body id="ho">
+        <?php
+        // put your code here
+        ?>
+        <a href="src/vendor/font-awesome/fonts/Amelia-Normal.ttf"></a>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a id="navbar-brand" class="navbar-brand" href="index.php">STS Developement</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Body Content -->
+        <!-- COVER -->
+        <div id="Cover" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div>
+                <p>Font Store</p>
+                <h1>Download the All Fonts You Need.</h1>
+                <br>
+                <a class="bi bi-arrow-down-circle-fill" href="#About"></a>
+            </div>
+        </div>
+
+        <!-- ABOUT -->
+        <div id="About" class="top col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div>
+                <h1>About Us</h1>
+                <h1 class="und">_&nbsp;&nbsp;&nbsp;&nbsp;_</h1>
+                <br>
+                <div>
+                    <p id="about-p">Hi, My Name is Shavindu Thushara Sampath. 
+                        This is a Web Application to Download the Font to help for your Day to Day Computer based Activies. 
+                        Here is Collection of populer fonts I ever Used. Credits of the this Fonts are goes to there Real Owners and Makers. 
+                        We are a team of font enthusiasts who believe that every design deserves a unique typeface. 
+                        <br><br>Our mission is to make it easy for designers to find and download high-quality, free fonts. 
+                        With a constantly growing collection of over [number] fonts, we strive to provide a diverse range of styles for every design project. 
+                        Whether you're working on a professional project or just having fun with typography, we have a font for you. 
+                        <br><br>Our website is user-friendly, with a simple interface that makes it easy to search for the perfect font. 
+                        We carefully curate our collection, ensuring that every font is of the highest quality and free for personal and commercial use. 
+                        With regular updates and new font releases, you're sure to find the right font for your project every time you visit. 
+                        <br><br>Thank you for choosing us as your font destination. We're excited to be a part of your design journey and can't wait to see what you create.
+                    </p>
+                </div>
+                <a class="bi bi-arrow-down-circle-fill" href="#features"></a>
+            </div>
+        </div>
+
+        <!-- features -->
+        <div id="features" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div>
+                <h1 class="top">Features</h1>
+                <h1 class="und">_&nbsp;&nbsp;&nbsp;&nbsp;_</h1>
+                <br>
+                <div>
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <center><img src="original.png" class="d-block " width="300px" alt="..."></center>
+                            </div> 
+                            <div class="carousel-item">
+                                <center><img src="Fast download.png" class="d-block" width="300px" alt="..."></center>
+                            </div>
+                            <div class="carousel-item">
+                                <center><img src="Totally Free.png" class="d-block" width="300px" alt="..."></center>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+
+
+                    <ul id="features-p">
+                        <div class="resume-item">
+                            <h5>Wide Selection : </h5>
+                            <li>Choose from a constantly growing collection of over [number] fonts, in a range of styles to suit every design project.</li>
+                        </div>
+                        <div class="resume-item">
+                            <h5>Easy Search : </h5>
+                            <li>Find the perfect font with our user-friendly interface and advanced search filters.</li>
+                        </div>
+                        <div class="resume-item">
+                            <h5>High Quality : </h5>
+                            <li>We carefully curate our collection, ensuring that every font is of the highest quality and free for personal and commercial use.</li>
+                        </div>
+                        <div class="resume-item">
+                            <h5>Regular Updates : </h5>
+                            <li>With new font releases and updates, you'll always have access to the latest and greatest font styles.</li>
+                        </div>
+                        <div class="resume-item">
+                            <h5>User Friendly : </h5>
+                            <li>Our simple, intuitive interface makes it easy to download and use the fonts you need.</li>
+                        </div>
+                        <div class="resume-item">
+                            <h5>Commercial Use : </h5>
+                            <li>All of our fonts are free for personal and commercial use, so you can use them in your projects without any restrictions.</li>
+                        </div>
+                        <div class="resume-item">
+                            <h5>No Sign Up Required : </h5>
+                            <li>Browse and download fonts without the hassle of creating an account.</li>
+                        </div>
+                    </ul>
+                </div>
+                <a class="bi bi-arrow-down-circle-fill" href="#fonts"></a>
+            </div>
+        </div>
+
+        <!-- font -->
+        <div id="fonts" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div>
+                <h1 class="top">Fonts</h1>
+                <h1 class="und">_&nbsp;&nbsp;&nbsp;&nbsp;_</h1>
+                <br>
+                <div>
+                    <p id="fonts-p">Hi, My Name is Shavindu Thushara Sampath. 
+                        This is a Web Application to Download the Font to help for your Day to Day Computer based Activies. 
+                        Here is Collection of populer fonts I ever Used. Credits of the this Fonts are goes to there Real Owners and Makers. 
+                        We are a team of font enthusiasts who believe that every design deserves a unique typeface. 
+                        <br><br>Our mission is to make it easy for designers to find and download high-quality, free fonts. 
+                        With a constantly growing collection of over [number] fonts, we strive to provide a diverse range of styles for every design project. 
+                        Whether you're working on a professional project or just having fun with typography, we have a font for you. 
+                        <br><br>Our website is user-friendly, with a simple interface that makes it easy to search for the perfect font. 
+                        We carefully curate our collection, ensuring that every font is of the highest quality and free for personal and commercial use. 
+                        With regular updates and new font releases, you're sure to find the right font for your project every time you visit. 
+                        <br><br>Thank you for choosing us as your font destination. We're excited to be a part of your design journey and can't wait to see what you create.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div id="footers">
+            <div>
+                <a class="bi bi-arrow-up-circle-fill" href="#ho"></a>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container-fluid">
+                <p>&copy; 2023 <i class="company">STS Development.</i> All rights reserved.</p>
+            </div>
+        </footer>
+    </body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
